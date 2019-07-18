29c1d1187b2d56fe364675cee632b5df8126a0d9
Plano Green Team Admin <49990972+planogreenteam@users.noreply.github.com>
2019-07-17 19:47:06 -0500

    Add files via upload

---
+++ solar_car_emulator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import yaml
+import time
+print('Welcome to the Simple Learning Solar Car')
+
+# setup
+
+# main loop
+while(True):
+    print('do stuff here')
+    time.sleep(0.5)
